5f35a12585f5edb43ab1da5edd8d05243da65a64
Juncheol Cho <zironycho@gmail.com>
2018-01-19 16:42:08 +0900

    release 0.0.6 (#46)
    * update docs due to text example; cleanup code
    * version up to 0.0.6

--- download-models.sh
+++ download-models.sh
@@ -1,11 +1,23 @@
+#!/bin/bash
 test_model_dir="./test/data"
 
-wget http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz
-wget http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
-wget https://raw.githubusercontent.com/darkonhub/darkon-examples/master/gradcam/sequence.tar
-
 mkdir -p $test_model_dir
-tar -xf resnet_v1_50_2016_08_28.tar.gz -C $test_model_dir
-tar -xf vgg_16_2016_08_28.tar.gz -C $test_model_dir
-tar -xf sequence.tar -C $test_model_dir
+
+resnet_file="resnet_v1_50_2016_08_28.tar.gz"
+if ! [ -f $resnet_file ]; then
+	wget http://download.tensorflow.org/models/$resnet_file
+	tar -xf $resnet_file -C $test_model_dir
+fi
+
+vgg_file="vgg_16_2016_08_28.tar.gz"
+if ! [ -f $vgg_file ]; then
+	wget http://download.tensorflow.org/models/$vgg_file
+	tar -xf $vgg_file -C $test_model_dir
+fi
+
+text_sequence_file="sequence.tar"
+if ! [ -f $text_sequence_file ]; then
+	wget https://raw.githubusercontent.com/darkonhub/darkon-examples/master/gradcam/$text_sequence_file
+	tar -xf $text_sequence_file -C $test_model_dir
+fi
 
