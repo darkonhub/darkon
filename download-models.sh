0bab4500b8c888cafa6243c7ce0e95d038bf593e
sunghwan <sunghwan@kaist.ac.kr>
2018-01-19 15:00:39 +0900

    add sequence model to download model script

--- download-models.sh
+++ download-models.sh
@@ -2,6 +2,10 @@ test_model_dir="./test/data"
 
 wget http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz
 wget http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
+wget https://raw.githubusercontent.com/darkonhub/darkon-examples/master/gradcam/sequence.tar
+
+mkdir -p $test_model_dir
 tar -xf resnet_v1_50_2016_08_28.tar.gz -C $test_model_dir
 tar -xf vgg_16_2016_08_28.tar.gz -C $test_model_dir
+tar -xf sequence.tar -C $test_model_dir
 
